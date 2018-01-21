bd2c062f132d6c914bf1aabb35e3b1a2cbc39027
jan_dev <jan_dev>
2018-01-21 20:53:25 +0100

    Change tileset and map!

---
+++ data/map/Sprite_lvl.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Sprite_lvl" tilewidth="32" tileheight="32" tilecount="13" columns="13">
+ <image source="data/img/map/Sprite_lvl.png" width="416" height="32"/>
+</tileset>
